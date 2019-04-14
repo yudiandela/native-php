fefa66b07a97fd49089c893c4e340a5c788a9c86
Yudi Andela <yudhi.andhela@gmail.com>
2019-04-14 15:11:48 +0700

    Mengenal Tipe Data di PHP

---
+++ latihan3/variable2.php
@@ -0,0 +1,26 @@
+<?php
+
+// tipe data string (teks)
+$nama_lengkap = "Yudi Andela";
+
+// tipe data char (karakter)
+$jenis_kelamin = 'L';
+
+// tipe data integer
+$umur = 30;
+
+// tipe data float
+$berat = 68.3;
+
+// tipe data float
+$tinggi = 182.2;
+
+// tipe data boolean
+$menikah = true;
+
+echo "Nama: $nama_lengkap<br>";
+echo "Jenis Kelamin: $jenis_kelamin<br>";
+echo "Umur: $umur tahun<br>";
+echo "berat badan: $berat kg<br>";
+echo "tinggi badan: $tinggi cm<br>";
+echo "menikah: $menikah";
